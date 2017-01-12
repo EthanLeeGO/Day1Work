69e41757604a00306176b74bf9e920e0bc126650
07477 <07477@07477PCWIN10>
2017-01-12 15:47:08 +0800

    fix

--- ConsoleApplication1/ConsoleApplication1/MyGroup.cs
+++ ConsoleApplication1/ConsoleApplication1/MyGroup.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
     public class MyGroup
     {
-        public List<int> GroupSum(List<int> values, int group)
+        public List<int> GroupSum(IEnumerable<int> values, int group)
         {
             List<int> GroupSum = new List<int>();
-            int div = values.Count / group;
-            if (values.Count % group > 0)
+            int div = values.Count() / group;
+            if (values.Count() % group > 0)
             {
                 div++;
             }
--- ConsoleApplication1/ConsoleApplication1Tests/MyGroupTests.cs
+++ ConsoleApplication1/ConsoleApplication1Tests/MyGroupTests.cs
@@ -1,24 +1,51 @@
 ﻿using ExpectedObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApplication1.Tests
 {
     [TestClass()]
     public class MyGroupTests
     {
+
+
+        public class Product
+        {
+            public int id { get; set; }
+            public int cost { get; set; }
+            public int Revenue { get; set; }
+            public int sellprice { get; set; }
+        }
+
+        public List<Product> produsts = new List<Product>();
+
+        public MyGroupTests()
+        {
+            produsts.Add(new Product() { id =1, cost = 1,Revenue=11,sellprice=21 });
+            produsts.Add(new Product() { id = 2, cost = 2, Revenue = 12, sellprice = 22 });
+            produsts.Add(new Product() { id = 3, cost = 3, Revenue = 13, sellprice = 23 });
+            produsts.Add(new Product() { id = 4, cost = 4, Revenue = 14, sellprice = 24 });
+            produsts.Add(new Product() { id = 5, cost = 5, Revenue = 15, sellprice = 25 });
+            produsts.Add(new Product() { id = 6, cost = 6, Revenue = 16, sellprice = 26 });
+            produsts.Add(new Product() { id = 7, cost = 7, Revenue = 17, sellprice = 27 });
+            produsts.Add(new Product() { id = 8, cost = 8, Revenue = 18, sellprice = 28 });
+            produsts.Add(new Product() { id = 9, cost = 9, Revenue = 19, sellprice = 29 });
+            produsts.Add(new Product() { id = 10, cost = 10, Revenue = 20, sellprice = 30 });
+            produsts.Add(new Product() { id = 11, cost = 11, Revenue = 21, sellprice = 31 });
+        }
+ 
+
+
         [TestMethod()]
+
         public void GroupSumTest_by_3_group()
         {
 
             var expected = new MyGroup();
-            var costs = new List<int>();
-            for (int i = 1; i <= 11; i++)
-            {
-                costs.Add(i);
-           } 
+           
             var group = 3;
-            var act = expected.GroupSum(costs, group);
+            var act = expected.GroupSum(produsts.Select(x=>x.cost) , group);
             var ass = new List<int>() { 6, 15, 24, 21 };
             // Assert.AreEqual(ass, act);
             act.ToExpectedObject().ShouldEqual(act);
@@ -30,12 +57,9 @@ namespace ConsoleApplication1.Tests
 
             var expected = new MyGroup();
             var costs = new List<int>();
-            for (int i = 11; i <= 21; i++)
-            {
-                costs.Add(i);
-            }
+       
             var group = 4;
-            var act = expected.GroupSum(costs, group);
+            var act = expected.GroupSum(produsts.Select(x => x.Revenue), group);
             var ass = new List<int>() { 50, 66, 601 };
             // Assert.AreEqual(ass, act);
             act.ToExpectedObject().ShouldEqual(act);
