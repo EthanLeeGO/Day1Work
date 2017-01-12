7768a8baa9ea66522766d37f8117c4e45690d0c8
07477 <07477@07477PCWIN10>
2017-01-12 10:48:38 +0800

    homework

---
+++ ConsoleApplication1/ConsoleApplication1/MyGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class MyGroup
+    {
+        public List<int> GroupSum(List<int> values, int group)
+        {
+            List<int> GroupSum = new List<int>();
+            int div = values.Count / group;
+            if (values.Count % group > 0)
+            {
+                div++;
+            }
+
+            for (int i = 1; i <= div; i++)
+            {
+                GroupSum.Add(values.Skip((i - 1) * group).Take(group).Sum());
+            }
+
+            return GroupSum;
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1Tests/MyGroupTests.cs
@@ -0,0 +1,44 @@
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace ConsoleApplication1.Tests
+{
+    [TestClass()]
+    public class MyGroupTests
+    {
+        [TestMethod()]
+        public void GroupSumTest_by_3_group()
+        {
+
+            var expected = new MyGroup();
+            var costs = new List<int>();
+            for (int i = 1; i <= 11; i++)
+            {
+                costs.Add(i);
+           } 
+            var group = 3;
+            var act = expected.GroupSum(costs, group);
+            var ass = new List<int>() { 6, 15, 24, 21 };
+            // Assert.AreEqual(ass, act);
+            act.ToExpectedObject().ShouldEqual(act);
+        }
+
+        [TestMethod()]
+        public void GroupSumTest_by_4_group()
+        {
+
+            var expected = new MyGroup();
+            var costs = new List<int>();
+            for (int i = 11; i <= 21; i++)
+            {
+                costs.Add(i);
+            }
+            var group = 4;
+            var act = expected.GroupSum(costs, group);
+            var ass = new List<int>() { 50, 66, 601 };
+            // Assert.AreEqual(ass, act);
+            act.ToExpectedObject().ShouldEqual(act);
+        }
+    }
+}
